9aa012238f2290de78cb8852110533d52048fcc0
Leonardo <37600692+uKaigo@users.noreply.github.com>
2022-04-11 17:13:08 -0300

    style: remove whitespace

--- .zsh/vikeys.zsh
+++ .zsh/vikeys.zsh
@@ -23,4 +23,3 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -v '^?' backward-delete-char
-
