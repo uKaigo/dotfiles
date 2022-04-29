f273a2886314a3b9ea5375a790b9308fda07f9a7
Leonardo <37600692+uKaigo@users.noreply.github.com>
2022-04-29 12:20:03 -0300

    feat(aliases): add bat -> cat alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -19,6 +19,10 @@ alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'
 
 alias diff="diff --color"
 
+if command -v bat > /dev/null; then
+  alias cat="bat -pp"
+fi
+
 # Bare dotfiles alias
 alias config='git --git-dir ${DOTFILES_HOME:-$HOME/dotfiles} --work-tree=$HOME'
 alias privconfig='git --git-dir ${DOTFILES_PRIVATE_HOME:-$HOME/dotfiles-private} --work-tree=$HOME'
