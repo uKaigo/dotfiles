6bab8afe2c975bfffe3e8c231fd29b5dcfc12f3e
Leonardo <37600692+uKaigo@users.noreply.github.com>
2022-09-21 11:38:52 -0300

    feat: remove unused aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,19 +1,8 @@
 alias scrcpy='scrcpy --prefer-text'
 
 alias ls='ls --color=tty -A'
-alias l='ls -lFh'
-alias lr='ls -tRFh'
-alias lt='ls -ltFh'
-alias ll='ls -l'
-alias ldot='ls -ld .*'
-alias lS='ls -1FSsh'
-alias lart='ls -1Fcart'
-alias lrt='ls -1Fcrt'
-alias lsr='ls -lARFh'
-alias lsn='ls -1'
 
 alias grep="grep --color"
-alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 
 alias diff="diff --color"
 
