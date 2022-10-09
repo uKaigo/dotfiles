ebe29f81de1ec2b418c7832caea0be83da6d14fc
Leonardo <37600692+uKaigo@users.noreply.github.com>
2022-10-09 18:06:36 -0300

    refactor(nvim): store options in object

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,10 +8,26 @@ require('onedark').setup {
 require('onedark').load()
 
 -- General options
-vim.o.number = true
-vim.o.mouse = 'h'
-vim.o.wrap = false
-vim.o.cursorline = true
+vim_options = {
+  number = true,
+  mouse = 'a', -- It's nice to have the option :)
+  wrap = false,
+  cursorline = true, -- Highlight current line
+  autoindent = true,
+  smartindent = true,
+  shiftwidth = 2, -- Number of spaces per indentation
+  ignorecase = true,
+  smartcase = true,
+  title = true,
+  numberwidth = 2,
+  signcolumn = "yes",
+  scrolloff = 4, -- Number of lines to keep above and below the cursor
+  sidescrolloff = 4, -- Number of lines to keep left and right of the cursor
+}
+
+for k, v in pairs(vim_options) do
+  vim.opt[k] = v
+end
 
 -- Load other scripts
 require('scripts.numbertoggle')
