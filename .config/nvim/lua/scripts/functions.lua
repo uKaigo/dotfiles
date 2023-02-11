6ff547075bbc9387e9700905732ad18c8f7e20ca
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-02-11 10:28:44 -0300

    refactor(functions.lua): move command to outer scope

--- .config/nvim/lua/scripts/functions.lua
+++ .config/nvim/lua/scripts/functions.lua
@@ -1,16 +1,18 @@
 local functions = {}
 
+local command = [[
+echohl ErrorMsg
+echo " "
+echo 'Failed to load "%s":'
+echo "%s"
+echohl None 
+]]
+
 -- Will require the path and print any errors without throwing.
 function functions.prequire(path)
   local ran, res = pcall(require, path)
   if not ran then
-    vim.cmd(([[
-              echohl ErrorMsg
-              echo " "
-              echo 'Failed to load "%s":'
-              echo "%s"
-              echohl None 
-	     ]]):format(path, res))
+    vim.cmd(command:format(path, res))
     return false
   end
   return res
