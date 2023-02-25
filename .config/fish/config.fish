f0db49217bebd6d9606542ed4001b73a7a65a14a
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-02-25 16:42:12 -0300

    feat(fish): add exa abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,5 @@ _run_if_found starship init fish | source
 _run_if_found rtx activate -s fish | source
 
 type -q bat && abbr cat 'bat -pp'
-type -q exa && abbr ls 'exa'
+type -q exa && abbr ls 'exa -a --icons'
+type -q exa && abbr exa 'exa -a --icons' # Abbreviations are not recursive.
