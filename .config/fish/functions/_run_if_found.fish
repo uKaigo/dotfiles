7733dba793827c96e51e2f61e120b440a93c791b
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-03-12 10:34:38 -0300

    refactor(_run_if_found): omit -1

--- .config/fish/functions/_run_if_found.fish
+++ .config/fish/functions/_run_if_found.fish
@@ -1,5 +1,5 @@
 function _run_if_found --description "Run the program if it's found. Intended for use in scripts."
   command -sq $argv[1] || return 0
-  command $argv[1] $argv[2..-1]
+  command $argv[1] $argv[2..]
   return $status
 end
