65ffae57bf9c7e15c7a591aff5120f70aa82dbc4
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-06-25 16:16:10 -0300

    feat!: pin rtx bin
    It may not be in PATH when first running

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ status is-interactive || exit 0
 
 _run_if_found starship init fish | source
 
-_run_if_found rtx activate -s fish | source
+_run_if_found "$HOME/.local/share/rtx/bin/rtx" activate -s fish | source
 
 # I'm hardcoding the bin folder because `yarn global bin`
 # takes about 500ms.
