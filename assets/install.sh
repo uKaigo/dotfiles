a48f4df215e0df2c1af40ffc43da1206816aa13d
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-06-25 17:23:13 -0300

    feat: update install script

--- assets/install.sh
+++ assets/install.sh
@@ -11,7 +11,7 @@ PURPLE='\033[1;35m'
 RESET='\033[m'
 
 inf()  {
-  printf "\033[1;35m%s\033[m\n" "$@"
+  printf "$PURPLE%s$RESET\n" "$@"
 }
 
 die()  {
@@ -20,64 +20,69 @@ die()  {
   exit $ret
 }
 
+dot()  {
+  git --git-dir="$DOTFILES_HOME" --work-tree="$HOME" "$@"
+}
+
 check_deps()  {
   for dep; do
     if ! command -v "$dep" > /dev/null; then
-      printf "$RED%s %s %s\n" "Program " "$dep" "not found. Please install it."
+      printf "$RED%s %s %s" "Program" "$dep" "not found. Install it or add to PATH."
       die
     fi
   done
 }
 
-clone_repo()  {
-  inf "Cloning repository..."
-  printf "$YELLOW"
+clone_repo() {
+  inf "Cloning the repository to '$DOTFILES_HOME'..."
   git clone --bare "https://github.com/uKaigo/dotfiles" "$DOTFILES_HOME" || die
-  printf "$RESET"
-  config config --local status.showUntrackedFiles no
-}
-
-clone_submodules()  {
-  inf "Cloning submodules..."
-  printf "$YELLOW"
-  config submodule init
-  config submodule update
-  printf "$RESET"
-}
-
-config()  {
-  git --git-dir="$DOTFILES_HOME" --work-tree="$HOME" "$@"
+  dot config --local status.showUntrackedFiles no
 }
 
 backup_files()  {
-  files=$(config ls-tree --name-only --full-tree -r HEAD)
+  files=$(dot ls-tree --name-only --full-tree -r HEAD)
   [ -n "$files" ] && inf "Backing up files..."
   for file in $files; do
     [ ! -f "$file" ] && continue
-    printf "$PURPLE%s \"$GREEN%s$PURPLE\" %s \"$GREEN%s$PURPLE\"%s$RESET\n" "Backing up" "$file" "to" "$file.bkp" "..."
+    printf "$PURPLE%s '$YELLOW%s$PURPLE' %s '$YELLOW%s$PURPLE'%s$RESET\n" "Backing up" "$file" "to" "$file.bkp" "..."
     mv "$HOME/$file" "$HOME/$file.bkp"
   done
 }
 
 checkout()  {
   inf "Checking out repository..."
-  printf "$YELLOW"
-  config checkout || die
-  printf "$RESET"
+  dot checkout || die
+}
+
+install_nvchad() {
+  inf "Do you want to install NvChad? [Y/n]"
+  read -n 1 -r 
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    inf "Installing NvChad to '$HOME/.config/nvim'..."
+    cd "$HOME/.config/nvim"
+    # https://stackoverflow.com/questions/2411031/how-do-i-clone-into-a-non-empty-directory 
+    git init
+    git remote add origin "https://github.com/NvChad/NvChad"
+    git fetch
+    # https://stackoverflow.com/questions/28666357/how-to-get-default-git-branch
+    branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
+    git checkout -t "origin/$branch"
+    cd - > /dev/null
+  fi
 }
 
 trap 'printf "\033[0m"; exit 1' INT HUP
 
-check_deps "git"
+check_deps "git" "sed"
 
 inf "Installing uKaigo's dotfiles..."
 
 clone_repo
 backup_files
 checkout
-clone_submodules
+install_nvchad
 
 inf "Installed."
-inf "Recommended packages: zsh, delta (dandavison/delta)"
 
 printf "$RESET"
