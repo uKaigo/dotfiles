2a820d9ddf89dbe167d39a37af3311a0b333f326
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-06-29 18:22:34 -0300

    feat!(nvim): make dotfiles-private hidden

--- .config/fish/functions/privdot.fish
+++ .config/fish/functions/privdot.fish
@@ -1,3 +1,3 @@
 function privdot --wraps='git' --description 'Alias to work with bare private-dotfiles repo.'
-  git --git-dir "$HOME/dotfiles-private" --work-tree "$HOME" $argv
+  git --git-dir "$HOME/.dotfiles-private" --work-tree "$HOME" $argv
 end
