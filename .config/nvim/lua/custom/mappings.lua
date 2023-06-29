23eea0626154421644d4416616d6b2232cb1c7b5
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-06-29 18:22:07 -0300

    feat(nvim): add mappings description

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,24 +2,24 @@ local M = {}
 
 M.move = {
   n = {
-    ["<A-j>"] = {"<cmd> m .+1 <CR>=="},
-    ["<A-k>"] = {"<cmd> m .-2 <CR>=="},
+    ["<A-j>"] = {"<cmd> m .+1 <CR>==", "Move 1 line down"},
+    ["<A-k>"] = {"<cmd> m .-2 <CR>==", "Move 1 line up"},
   },
   i = {
-    ["<A-j>"] = {"<Esc> <cmd> m .+1 <CR>==gi"},
-    ["<A-k>"] = {"<Esc> <cmd> m .-2 <CR>==gi"},
+    ["<A-j>"] = {"<Esc> <cmd> m .+1 <CR>==gi", "Move 1 line down"},
+    ["<A-k>"] = {"<Esc> <cmd> m .-2 <CR>==gi", "Move 1 line up"},
   },
   v = {
     -- The colon is needed for the mark to be set.
-    ["<A-j>"] = {":m '>+1 <CR>gv=gv"},
-    ["<A-k>"] = {":m '<-2 <CR>gv=gv"}
+    ["<A-j>"] = {":m '>+1 <CR>gv=gv", "Move block 1 line down"},
+    ["<A-k>"] = {":m '<-2 <CR>gv=gv", "Move block 1 line up"}
   }
 }
 
 M.dap = {
   plugin = true,
   n = {
-    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>", "Add debug breakpoint"}
   }
 }
 
@@ -29,7 +29,8 @@ M.dap_python = {
     ["<leader>dpr"] = {
       function()
         require('dap-python').test_method()
-      end
+      end,
+      "Debug python file"
     }
   }
 }
