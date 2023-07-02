8c6967e5dc1a9bbc3075346e3d41086f1e36a2b2
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-07-02 18:08:54 -0300

    feat!(nvim): reorganize plugins and remove unneeded

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,4 +1,11 @@
 local P = {
+  -- Debugger
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings "dap"
+    end,
+  },
   {
     "rcarriga/nvim-dap-ui",
     dependencies = "mfussenegger/nvim-dap",
@@ -18,12 +25,6 @@ local P = {
     end,
   },
   {
-    "mfussenegger/nvim-dap",
-    config = function(_, _)
-      require("core.utils").load_mappings "dap"
-    end,
-  },
-  {
     "mfussenegger/nvim-dap-python",
     ft = "python",
     dependencies = {
@@ -36,26 +37,8 @@ local P = {
       require("core.utils").load_mappings "dap_python"
     end,
   },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = { "python", "lua" },
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "blue",
-        "stylua",
-        "pyright",
-        "ruff",
-        "debugpy",
-        "rust_analyzer",
-      },
-    },
-  },
+
+  -- LSP
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -64,13 +47,11 @@ local P = {
     end,
   },
   {
-    "rgroli/other.nvim",
-    cmd = { "Other", "OtherSplit", "OtherVSplit" },
-    keys = {
-      { "<leader>of", "<cmd> Other <CR>", desc = "Open Other file" },
-      { "<leader>os", "<cmd> OtherSplit <CR>", desc = "Open Other file in horizontal split" },
-      { "<leader>ov", "<cmd> OtherVSplit <CR>", desc = "Open Other file in vertical split" },
-    },
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = { "python", "lua" },
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
   },
   {
     "rust-lang/rust.vim",
@@ -90,21 +71,30 @@ local P = {
       require("rust-tools").setup(opts)
     end,
   },
+
+  -- Other
   {
-    "saecki/crates.nvim",
-    ft = { "rust", "toml" },
-    config = function(_, opts)
-      local crates = require "crates"
-      crates.setup(opts)
-      crates.show()
-    end,
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "blue",
+        "stylua",
+        "pyright",
+        "ruff",
+        "debugpy",
+        "rust_analyzer",
+      },
+    },
   },
+
   {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local M = require "plugins.configs.cmp"
-      table.insert(M.sources, { name = "crates" })
-    end,
+    "rgroli/other.nvim",
+    cmd = { "Other", "OtherSplit", "OtherVSplit" },
+    keys = {
+      { "<leader>of", "<cmd> Other <CR>", desc = "Open Other file" },
+      { "<leader>os", "<cmd> OtherSplit <CR>", desc = "Open Other file in horizontal split" },
+      { "<leader>ov", "<cmd> OtherVSplit <CR>", desc = "Open Other file in vertical split" },
+    },
   },
 }
 return P
