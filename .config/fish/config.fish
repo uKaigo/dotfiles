c776aa705741e61fddb6e0e20fee67b379df7600
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-07-13 12:00:44 -0300

    feat(fish): add nvsu abbr
    Since I don't have my configuration in root

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ _run_if_found "$HOME/.local/share/rtx/bin/rtx" activate -s fish | source
 fish_add_path -P "$HOME/.yarn/bin"
 
 alias docker='sudo docker'
+type -q nvim && abbr nvsu 'sudo env HOME=$HOME nvim'
 type -q bat && abbr cat 'bat -pp'
 type -q exa && abbr ls 'exa -a --icons'
 type -q exa && abbr exa 'exa --icons' # Abbreviations are not recursive.
