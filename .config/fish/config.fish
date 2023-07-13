a8528e2e8174dc4f6577e33d9c14f7cbe60b8ee4
Leonardo <37600692+uKaigo@users.noreply.github.com>
2023-07-13 12:08:39 -0300

    perf!(fish): remove checking for command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,8 @@ _run_if_found "$HOME/.local/share/rtx/bin/rtx" activate -s fish | source
 # takes about 500ms.
 fish_add_path -P "$HOME/.yarn/bin"
 
-type -q docker && abbr docker 'sudo docker'
-type -q nvim && abbr nvsu 'sudo env HOME=$HOME nvim'
-type -q bat && abbr cat 'bat -pp'
-type -q exa && abbr ls 'exa -a --icons'
-type -q exa && abbr exa 'exa --icons' # Abbreviations are not recursive.
+abbr docker 'sudo docker'
+abbr nvsu 'sudo env HOME=$HOME nvim'
+abbr cat 'bat -pp'
+abbr ls 'exa -a --icons'
+abbr exa 'exa --icons' # Abbreviations are not recursive.
