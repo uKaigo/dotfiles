b5d5312cbb5af03794fbe3be9407da533347cc4f
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-01-03 16:52:12 -0300

    fix(mitm*): infinite recursion

--- .config/fish/functions/mitmproxy.fish
+++ .config/fish/functions/mitmproxy.fish
@@ -1,4 +1,4 @@
 function mitmproxy --wraps='mitmproxy' --description 'Alias to change mitmproxy config dir.'
   set -q MITMPROXY_HOME; or set MITMPROXY_HOME "$HOME/.mitmproxy"
-  mitmproxy --set confdir="$MITMPROXY_HOME"
+  command mitmproxy --set confdir="$MITMPROXY_HOME"
 end
--- .config/fish/functions/mitmweb.fish
+++ .config/fish/functions/mitmweb.fish
@@ -1,4 +1,4 @@
 function mitmweb --wraps='mitmweb' --description 'Alias to change mitmweb config dir.'
   set -q MITMPROXY_HOME; or set MITMPROXY_HOME "$HOME/.mitmproxy"
-  mitmweb --set confdir="$MITMPROXY_HOME" $argv
+  command mitmweb --set confdir="$MITMPROXY_HOME" $argv
 end
