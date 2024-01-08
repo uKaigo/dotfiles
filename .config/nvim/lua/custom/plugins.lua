5ff3829ae809121b84757fc456c5bcc7dc25b9a6
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-01-08 19:51:55 -0300

    feat!(nvim): remove unused plugins

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,42 +1,42 @@
 local P = {
   -- Debugger
-  {
-    "mfussenegger/nvim-dap",
-    config = function(_, _)
-      require("core.utils").load_mappings "dap"
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require "dap"
-      local dapui = require "dapui"
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-    },
-    config = function(_, _)
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-      require("core.utils").load_mappings "dap_python"
-    end,
-  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   config = function(_, _)
+  --     require("core.utils").load_mappings "dap"
+  --   end,
+  -- },
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   dependencies = "mfussenegger/nvim-dap",
+  --   config = function()
+  --     local dap = require "dap"
+  --     local dapui = require "dapui"
+  --     dapui.setup()
+  --     dap.listeners.after.event_initialized["dapui_config"] = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.event_terminated["dapui_config"] = function()
+  --       dapui.close()
+  --     end
+  --     dap.listeners.before.event_exited["dapui_config"] = function()
+  --       dapui.close()
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "mfussenegger/nvim-dap-python",
+  --   ft = "python",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "rcarriga/nvim-dap-ui",
+  --   },
+  --   config = function(_, _)
+  --     local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+  --     require("dap-python").setup(path)
+  --     require("core.utils").load_mappings "dap_python"
+  --   end,
+  -- },
 
   -- LSP
   {
@@ -93,15 +93,15 @@ local P = {
     opts = require "custom.configs.nvim-tree",
   },
 
-  {
-    "rgroli/other.nvim",
-    cmd = { "Other", "OtherSplit", "OtherVSplit" },
-    keys = {
-      { "<leader>of", "<cmd> Other <CR>", desc = "Open Other file" },
-      { "<leader>os", "<cmd> OtherSplit <CR>", desc = "Open Other file in horizontal split" },
-      { "<leader>ov", "<cmd> OtherVSplit <CR>", desc = "Open Other file in vertical split" },
-    },
-  },
+  -- {
+  --   "rgroli/other.nvim",
+  --   cmd = { "Other", "OtherSplit", "OtherVSplit" },
+  --   keys = {
+  --     { "<leader>of", "<cmd> Other <CR>", desc = "Open Other file" },
+  --     { "<leader>os", "<cmd> OtherSplit <CR>", desc = "Open Other file in horizontal split" },
+  --     { "<leader>ov", "<cmd> OtherVSplit <CR>", desc = "Open Other file in vertical split" },
+  --   },
+  -- },
 
   -- {
   --   "christoomey/vim-tmux-navigator",
