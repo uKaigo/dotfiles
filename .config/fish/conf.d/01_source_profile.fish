88394d9acaa51621514359b25ea57c598b7278c2
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-01-09 17:21:14 -0300

    feat(fish): source .profile

---
+++ .config/fish/conf.d/01_source_profile.fish
@@ -0,0 +1,1 @@
+source "$HOME/.profile" # Will error if profile includes more than env vars.
--- .config/fish/conf.d/01_start_tmux.fish
+++
@@ -1,5 +0,0 @@
-# start_tmux > /dev/null
-# if [ $status -eq 0 ]
-#   # Odd way of exitting, but "exit" does not work for some reason.
-#   exec true
-# end
