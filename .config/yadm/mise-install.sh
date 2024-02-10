c69b9332f2809ad13b33e080778a55651dad76a7
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-02-09 23:16:45 -0300

    chore: update mise

--- .config/yadm/mise-install.sh
+++ .config/yadm/mise-install.sh
@@ -41,11 +41,22 @@ get_os() {
 }
 
 get_arch() {
+	musl=""
+	if type ldd >/dev/null 2>/dev/null; then
+		libc=$(ldd /bin/ls | grep 'musl' | head -1 | cut -d ' ' -f1)
+		if [ -z "$libc" ]; then
+			musl="-musl"
+		fi
+	fi
 	arch="$(uname -m)"
 	if [ "$arch" = x86_64 ]; then
-		echo "x64"
+		echo "x64$musl"
 	elif [ "$arch" = aarch64 ] || [ "$arch" = arm64 ]; then
-		echo "arm64"
+		echo "arm64$musl"
+	elif [ "$arch" = armv6l ]; then
+		echo "armv6$musl"
+	elif [ "$arch" = armv7l ]; then
+		echo "armv7$musl"
 	else
 		error "unsupported architecture: $arch"
 	fi
@@ -65,19 +76,45 @@ get_checksum() {
 	os="$(get_os)"
 	arch="$(get_arch)"
 
-	checksum_linux_x86_64="9e2b02fabff6186951d99b0feff63793cfdda4c69df4d0fc89b2f129f76bfbeb  ./mise-v2024.1.22-linux-x64.tar.gz"
-	checksum_linux_arm64="56bb4c4a53b9e147e9d9760d9258b143948b26bd90de655f116aaa72f6191a41  ./mise-v2024.1.22-linux-arm64.tar.gz"
-	checksum_macos_x86_64="04a2be1fcd941ce4f7bcc0f9da93f06dbec0ce3c70f61a1248055dbc726c9000  ./mise-v2024.1.22-macos-x64.tar.gz"
-	checksum_macos_arm64="54ef7fc7e78504d67c936aa9c55389b8718250fd9ea98acd9a4647bd55d625e3  ./mise-v2024.1.22-macos-arm64.tar.gz"
+	checksum_linux_x86_64="4630aa9ac85b095f875284a6ccfbb7c90b4546ef988e69f3c0af8c158bbee936  ./mise-v2024.2.9-linux-x64.tar.gz"
+	checksum_linux_x86_64_musl="f9a8e051ceb9378b0ae2bc816bbc4e8ef95b67bfbe35ce023d71f561e3a2311e  ./mise-v2024.2.9-linux-x64-musl.tar.gz"
+	checksum_linux_arm64="c27f86a1030448c10702c5a44e00fd10d82f6bb6696923bff36d84391b686aab  ./mise-v2024.2.9-linux-arm64.tar.gz"
+	checksum_linux_arm64_musl="ad13f902e3672bd06bfb6f7593380901a43d3bc6b95f02df24b1b5ca80fd2e7c  ./mise-v2024.2.9-linux-arm64-musl.tar.gz"
+	checksum_linux_armv6="38ea5e00ff868a5324f334a42a067a467ed83e5610616f790da8d73a87e725a9  ./mise-v2024.2.9-linux-armv6.tar.gz"
+	checksum_linux_armv6_musl="ef2a82889b9b0248772e13eee11a00485257c4b36f0b7d08e7b3220f0f4549fd  ./mise-v2024.2.9-linux-armv6-musl.tar.gz"
+	checksum_linux_armv7="f5551ae704d70e21663ba6903d2aaa353a54e81d6566db66c06c473cb1cb1503  ./mise-v2024.2.9-linux-armv7.tar.gz"
+	checksum_linux_armv7_musl="37cb6793e438c4ff50fa727c97511ac83d3845596adf6d7153bfbb2fec078769  ./mise-v2024.2.9-linux-armv7-musl.tar.gz"
+	checksum_macos_x86_64="126faf0a4b1dd7ec0b75ec93ee69dcc75d2499910c6c9253055d0217a060e8ca  ./mise-v2024.2.9-macos-x64.tar.gz"
+	checksum_macos_arm64="c2ea89a643d89338d1fc9572ad906e950ee6dab55de9dc01a74c8c61c8b95bc7  ./mise-v2024.2.9-macos-arm64.tar.gz"
 
-	if [ "$os" = "linux" ] && [ "$arch" = "x64" ]; then
-		echo "$checksum_linux_x86_64"
-	elif [ "$os" = "linux" ] && [ "$arch" = "arm64" ]; then
-		echo "$checksum_linux_arm64"
-	elif [ "$os" = "macos" ] && [ "$arch" = "x64" ]; then
-		echo "$checksum_macos_x86_64"
-	elif [ "$os" = "macos" ] && [ "$arch" = "arm64" ]; then
-		echo "$checksum_macos_arm64"
+	if [ "$os" = "linux" ]; then
+		if [ "$arch" = "x64" ]; then
+			echo "$checksum_linux_x86_64"
+		elif [ "$arch" = "x64-musl" ]; then
+			echo "$checksum_linux_x86_64_musl"
+		elif [ "$arch" = "arm64" ]; then
+			echo "$checksum_linux_arm64"
+		elif [ "$arch" = "arm64-musl" ]; then
+			echo "$checksum_linux_arm64_musl"
+		elif [ "$arch" = "armv6" ]; then
+			echo "$checksum_linux_armv6"
+		elif [ "$arch" = "armv6-musl" ]; then
+			echo "$checksum_linux_armv6_musl"
+		elif [ "$arch" = "armv7" ]; then
+			echo "$checksum_linux_armv7"
+		elif [ "$arch" = "armv7-musl" ]; then
+			echo "$checksum_linux_armv7_musl"
+		else
+			warn "no checksum for $os-$arch"
+		fi
+	elif [ "$os" = "macos" ]; then
+		if [ "$arch" = "x64" ]; then
+			echo "$checksum_macos_x86_64"
+		elif [ "$arch" = "arm64" ]; then
+			echo "$checksum_macos_arm64"
+		else
+			warn "no checksum for $os-$arch"
+		fi
 	else
 		warn "no checksum for $os-$arch"
 	fi
@@ -94,8 +131,8 @@ download_file() {
 	info "mise: installing mise..."
 
 	if command -v curl >/dev/null 2>&1; then
-		debug ">" curl -fLlSso "$file" "$url"
-		curl -fLlSso "$file" "$url"
+		debug ">" curl -#fLo "$file" "$url"
+		curl -#fLo "$file" "$url"
 	else
 		if command -v wget >/dev/null 2>&1; then
 			debug ">" wget -qO "$file" "$url"
@@ -111,7 +148,7 @@ download_file() {
 
 install_mise() {
 	# download the tarball
-	version="v2024.1.22"
+	version="v2024.2.9"
 	os="$(get_os)"
 	arch="$(get_arch)"
 	install_path="${MISE_INSTALL_PATH:-$HOME/.local/bin/mise}"
@@ -163,3 +200,4 @@ after_finish_help() {
 }
 
 install_mise
+after_finish_help
