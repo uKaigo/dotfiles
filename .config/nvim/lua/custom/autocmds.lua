e292f9e929f270b6206a0ce8871e9d67dab8158b
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-02-29 11:50:02 -0300

    feat(nvim): hide tmux status when entering
    This does not look good if nvim is in a panel, but it's ok

--- .config/nvim/lua/custom/autocmds.lua
+++ .config/nvim/lua/custom/autocmds.lua
@@ -1,5 +1,6 @@
 -- Automatically change relativenumber depending on environment
 local NumberToggle = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
+local VimLife = vim.api.nvim_create_augroup("VimLife", { clear = true })
 
 vim.api.nvim_create_autocmd(
   { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
@@ -8,4 +9,13 @@ vim.api.nvim_create_autocmd(
 vim.api.nvim_create_autocmd(
   { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
   { command = [[if &nu | set nornu | endif]], group = NumberToggle }
+)
+
+vim.api.nvim_create_autocmd(
+  { "VimEnter" },
+  { command = 'call jobstart("tmux set status off")', once = true, group = VimLife }
+)
+vim.api.nvim_create_autocmd(
+  { "VimLeave" },
+  { command = 'silent exec "!tmux set status on"', once = true, group = VimLife }
 )
