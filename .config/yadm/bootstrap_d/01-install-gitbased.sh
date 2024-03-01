685c120190e2ef58fad53cf2ff1e0c3fc00db1d6
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-03-01 10:18:53 -0300

    feat(yadm_bootstrap): install tpm

--- .config/yadm/bootstrap_d/01-install-gitbased.sh
+++ .config/yadm/bootstrap_d/01-install-gitbased.sh
@@ -6,24 +6,38 @@ FILE_PATH="${0%/*}"
 source "$FILE_PATH/shared/ba.sh"
 
 NVIM_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+TMUX_PHOME="$HOME/.config/tmux/plugins" # Tmux does not respect XDG_CONFIG_HOME
 
-if ! confirm "Install NVChad"; then
-	exit 0
-fi
+install_nvchad() {
+	if ! confirm "Install NvChad"; then return; fi
 
-inf "Installing NvChad to '$NVIM_HOME'..."
-cd "$NVIM_HOME"
+	inf "Installing NvChad to '$NVIM_HOME'..."
+	pushd "$NVIM_HOME"
 
-git init >/dev/null
-if ! git remote add origin 'https://github.com/NvChad/NvChad' >/dev/null; then
-	inf "Couldn't add origin. Is NvChad already installed? Aborting..."
-	exit 0
-fi
-git fetch --depth=1 >/dev/null
+	git init >/dev/null
+	if ! git remote add origin 'https://github.com/NvChad/NvChad' >/dev/null; then
+		inf "Couldn't add origin. Is NvChad already installed? Aborting..."
+		return
+	fi
+	git fetch --depth=1 >/dev/null
 
-# https://stackoverflow.com/questions/28666357/how-to-get-default-git-branch
-branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-git checkout -t "origin/$branch" >/dev/null
-cd - >/dev/null
+	# https://stackoverflow.com/questions/28666357/how-to-get-default-git-branch
+	branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
+	git checkout -t "origin/$branch" >/dev/null
 
-inf "NvChad installed."
+	popd >/dev/null
+	inf "NvChad installed."
+}
+
+install_tpm() {
+	if ! confirm "Install Tmux Plugin Manager"; then return; fi
+
+	inf "Installing TPM to '$TMUX_PHOME'.."
+
+	git clone 'https://github.com/tmux-plugins/tpm' "$TMUX_PHOME/tpm" >/dev/null
+
+	inf "TPM Installed."
+}
+
+install_nvchad
+install_tpm
