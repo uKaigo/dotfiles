80579c020c92c5ee85798467183b71c41b4534cd
Leonardo <37600692+uKaigo@users.noreply.github.com>
2024-03-12 18:44:49 -0300

    feat(fish): use zoxide

--- .config/fish/conf.d/01_source_scripts.fish
+++ .config/fish/conf.d/01_source_scripts.fish
@@ -2,6 +2,8 @@ _run_if_found starship init fish | source
 
 _run_if_found "$HOME/.local/bin/mise" activate fish | source
 
+_run_if_found zoxide init fish --cmd cd | source
+
 if test -z "$__PROFILE_SOURCED"
   source ~/.profile
 end
