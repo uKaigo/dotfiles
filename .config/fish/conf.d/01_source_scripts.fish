027421f8230bf9b1a04f034e053c7c52170ae826
Leonardo <37600692+uKaigo@users.noreply.github.com>
2025-02-01 21:25:08 -0300

    feat(fish): add cargo env

--- .config/fish/conf.d/01_source_scripts.fish
+++ .config/fish/conf.d/01_source_scripts.fish
@@ -4,6 +4,10 @@ _run_if_found "$HOME/.local/bin/mise" activate fish | source
 
 _run_if_found zoxide init fish --cmd cd | source
 
+if test -f "$HOME/.cargo/env.fish"
+  source "$HOME/.cargo/env.fish"
+end
+
 if test -z "$__PROFILE_SOURCED"
   source ~/.profile
 end
