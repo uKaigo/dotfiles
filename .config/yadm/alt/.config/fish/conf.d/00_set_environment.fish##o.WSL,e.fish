53962ec1ea59c0cb5db45b24a438598489e16505
Leonardo <37600692+uKaigo@users.noreply.github.com>
2025-04-21 18:49:14 -0300

    fix(fish): fix a bug when the etc/environment is empty
    This causes export to be called without arguments, dumping the current environment.

--- .config/yadm/alt/.config/fish/conf.d/00_set_environment.fish##o.WSL,e.fish
+++ .config/yadm/alt/.config/fish/conf.d/00_set_environment.fish##o.WSL,e.fish
@@ -1,6 +1,9 @@
 # Loads /etc/environment. Should only be run by wsl, since it does not do it automatically.
-export (grep -o '^[^#]*' </etc/environment)
+set data (grep -o '^[^#]*' </etc/environment)
+
+test -n $data && export $data
 
 for file in /etc/environment.d/*
-    export (grep -o '^[^#]*' <$file)
+    set data (grep -o '^[^#]*' <$file)
+    test -n $data && export $data
 end
